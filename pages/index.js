b6f5aab160feb236d2fed669b369ea4e85dc92a5
casdidier <cheung.didier@gmail.com>
2021-02-16 13:35:11 +0400

    Fetch data with a GraphQL query in Next.js using Apollo Client

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+
+
 export default function Home({ launches }) {
   console.log('launches', launches);
 
@@ -67,9 +71,40 @@ export default function Home({ launches }) {
 }
 
 export async function getStaticProps() {
+
+  const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql/',
+    cache: new InMemoryCache()
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      query GetLaunches {
+        launchesPast(limit: 10) {
+          id
+          mission_name
+          launch_date_local
+          launch_site {
+            site_name_long
+          }
+          links {
+            article_link
+            video_link
+            mission_patch
+          }
+          rocket {
+            rocket_name
+          }
+        }
+      }
+    `
+  });
+
+  // console.log(data);
+
   return {
     props: {
-      launches: []
+      launches: data.launchesPast
     }
   }
 }
