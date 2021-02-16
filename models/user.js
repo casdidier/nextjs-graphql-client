2176fab7f38572e04eb518ef3b36dcc34bd24ea4
casdidier <cheung.didier@gmail.com>
2021-02-16 15:03:14 +0400

    adding the associations

--- models/user.js
+++ models/user.js
@@ -9,14 +9,26 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    // one-to-many relationships between our models
     static associate(models) {
-      // define association here
+      User.associate = function (models) {
+        User.hasMany(models.Recipe)
+      }
     }
   };
   User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'User',
